5f7823585cce291d1e9f1e8058fdd64d9889a65c
Rich Caloggero <rjc@mit.edu>
2022-04-06 12:30:40 -0400

    * Help text now included in commands object
    * null display taken into consideration when initializing values in keyboardHandler()

--- commands.js
+++ commands.js
@@ -1,67 +1,123 @@
 import {KeyboardKey} from "./keyboard.js";
 import {wrap} from "./grid.js";
+import {$} from "./dom.js";
 
 export function keyboardHandler(e, display) {
 const key = new KeyboardKey(e).toString();
-const buffer = display.buffer;
-const size = display.size;
+const buffer = display?.buffer;
+const size = display?.size || 1;
 const cellCount = size*size;
-const index = display.checkboxMap.get(display.getFocus());
+const index = display?.checkboxMap.get(display.getFocus()) || 0;
 const rowIndex = Math.floor(index/size);
 const columnIndex = Math.floor(index%size);
 
 
 const commands = {
-"F1": help,
-"?": help,
-
-"control g": function navigateToGrid () {display.focus(display.getFocus());},
-
-"control Home": function firstCell () {display.focus(buffer[0]);},
-"control End": function lastCell () {display.focus(buffer[cellCount-1]);},
-"Home": function rowStart () {display.focus(buffer[rowIndex * size]);},
-"End": function rowEnd () {display.focus(buffer[rowIndex * size + size-1]);},
-
-"alt ArrowUp": function previousRow () {
-if (index >= size) display.focus(buffer[index-size]);
+"F1": {
+description: "Show keyboard help",
+command: help
 },
-"ArrowUp": function previousRowWrap () {
-display.focus(buffer[wrap(index-size, cellCount-1)]);
+"?": {
+description: "Show keyboard help",
+command: help
 },
 
-"alt ArrowDown": function nextRow () {
-if (index+size < cellCount) display.focus(buffer[index+size]);
-},
-"ArrowDown": function nextRowWrap () {
-display.focus(buffer[wrap(index+size, cellCount-1)]);
-},
-
-"alt ArrowLeft": function previousColumn () {
-if (index%size > 0) display.focus(previousCell(index));
-},
-"control ArrowLeft": function previousLiveCell () {
-display.focus(previousLiveCell(index));
-}, "ArrowLeft": function previousColumnWrap () {
-display.focus(buffer[wrap(index-1, cellCount-1)]);
-},
-"alt ArrowRight": function nextColumn () {
-if (index%size < size-1) display.focus(nextCell(index));
-},
-"Control ArrowRight": function nextLiveCell () {
-display.focus(nextLiveCell(index));
-},
-"ArrowRight": function nextColumnWrap () {
-display.focus(buffer[wrap(index+1, cellCount-1)]);
-},
-
-"control Enter": function sonifyPosition () {
+"control Enter": {
+description: "Sonify current position in grid",
+command: function sonifyPosition () {
 life.sonifyPosition(index);
-}, // sonifyPosition
+} // sonifyPosition
+},
+
+"control g": {
+description: "Shift focus to grid",
+command: function navigateToGrid () {display.focus(display.getFocus());}
+},
+"control Home": {
+description: "Navigate to first cell in grid",
+command: function firstCell () {display.focus(buffer[0]);}
+},
+"control End": {
+description: "Navigate to last cell in grid",
+command: function lastCell () {display.focus(buffer[cellCount-1]);}
+},
+"Home": {
+description: "Navigate to start of current row",
+command: function rowStart () {display.focus(buffer[rowIndex * size]);}
+},
+"End": {
+description: "Navigate to end of current row",
+command: function rowEnd () {display.focus(buffer[rowIndex * size + size-1]);}
+},
+
+"alt ArrowUp": {
+description: "Navigate to previous row, stopping at first row",
+command: function previousRow () {
+if (index >= size) display.focus(buffer[index-size]);
+} // previousRow
+},
+"ArrowUp": {
+description: "Move to previous row with wrap",
+command: function previousRowWrap () {
+display.focus(buffer[wrap(index-size, cellCount-1)]);
+} // previousRowWrap 
+},
+
+"alt ArrowDown": {
+description: "Move to next row",
+command: function nextRow () {
+if (index+size < cellCount) display.focus(buffer[index+size]);
+} // nextRow
+},
+"ArrowDown": {
+description: "Move to next row with wrap",
+command: function nextRowWrap () {
+display.focus(buffer[wrap(index+size, cellCount-1)]);
+} // nextRowWrap
+},
+
+"alt ArrowLeft": {
+description: "Move to previous column",
+command: function previousColumn () {
+if (index%size > 0) display.focus(previousCell(index));
+} // previousColumn
+},
+"control ArrowLeft": {
+description: "Move to previous live cell",
+command: function previousLiveCell () {
+display.focus(previousLiveCell(index));
+} // previousLiveCell
+},
+"ArrowLeft": {
+description: "Move to previous column with wrap",
+command: function previousColumnWrap () {
+display.focus(buffer[wrap(index-1, cellCount-1)]);
+} // previousColumnWrap
+},
+
+"alt ArrowRight": {
+description: "Move to next column",
+command: function nextColumn () {
+if (index%size < size-1) display.focus(nextCell(index));
+} // nextColumn
+},
+"Control ArrowRight": {
+description: "Move to next live cell",
+command: function nextLiveCell () {
+display.focus(nextLiveCell(index));
+} // nextLiveCell
+},
+"ArrowRight": {
+description: "Move to next column with wrap",
+command: function nextColumnWrap () {
+display.focus(buffer[wrap(index+1, cellCount-1)]);
+} // nextColumnWrap
+},
 }; // commands
 
-if (key && commands[key] instanceof Function) {
+if (key && commands[key]?.command instanceof Function) {
 e.preventDefault();
-commands[key]();
+commands[key].command();
 } // if
 
 function nextCell (start) {return start<buffer.length-1? buffer[start+1] : buffer[start];}
@@ -77,23 +133,21 @@ return cell? cell : buffer[start];
 } // previousLiveCell
 
 function help () {
-const text = `<table>
-<tr><th>key</th> <th>Description</th></tr>
-<tr><th>alt + arrow</th>
-<td>move in indicated direction, stopping at boundaries</td>
-</tr><tr>
-<th>unmodified arrow</th>
-<td>move in indicated direction, continuing across row boundaries, wrapping at first / last cell</td>
-</tr><tr>
-<th>control + right / left arrows</th>
-<td>next / previous live cell, continuing across row boundaries, no wrap</td>
-</tr><tr>
-<th>control + enter</th>
-<td>Play a sound corresponding to the grid position of currently focused cell</td>
-</tr></table>
-`; // text
+const text = `
+<table><tr>
+<th>Key</th>
+<th>Description</th>
+</tr>
+${getCommandDescriptions(commands).map(entry =>
+`<tr>
+<th>${entry[0]}</th>
+<td>${entry[1]}</td>
+</tr>`)
+.join("")}
+</table>
+`;
 
-const modal = `<div class="modal" role="dialog" aria-model="true" aria-labelledby="help-modal-title">
+const modal = `<div class="modal" role="dialog" aria-modal="true" aria-labelledby="help-modal-title">
 <div class="header">
 <h3>Grid Navigation</h3>
 <button class="close">Close</button>
@@ -115,6 +169,11 @@ if (e.key === "Escape") e.target.click();
 });
 
 $(".close", container).focus();
+
+function getCommandDescriptions (commands) {
+return Object.keys(commands).map(key => [key, commands[key].description]);
+} // getCommandDescriptions
+
 } // help
 
 } // keyboardHandler
